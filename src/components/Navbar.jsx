fa0ee71b9859b38cec9e1496e1aa15cb190a96fa
jorojojojo23 <jbarres@uc.cl>
2025-05-26 14:52:13 -0400

    Add Navbar styles with background and decorative mask

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,29 @@
-
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file for styles
 
 function Navbar() {
   return (
-    <nav className="w-full p-4 bg-surface text-text [background-color:var(--color-background)]">
-      <ul className="flex gap-7 justify-center">
-      <li>
-        <Link to="/" className="text-primary font-semibold">Home</Link>
-      </li>
-      <li>
-        <Link to="/about" className="text-primary font-semibold">About Us</Link>
-      </li>
-      <li>
-        <Link to="/recetas" className="text-primary font-semibold">Recetas</Link>
-      </li>
-      <li>
-        <Link to="/order" className="text-primary font-semibold">Realizar Pedido</Link>
-      </li>
-      <li>
-        {/* <Link to="/temas" className="text-primary font-semibold">Theme</Link> */}
-      </li>
+    <nav className="relative w-full p-4 bg-surface text-text [background-color:var(--color-background)] navbar">
+      <ul className="flex gap-7 justify-center items-center">
+        <li>
+          <Link to="/" className="text-secondary font-semibold">Home</Link>
+        </li>
+        <li>
+          <Link to="/about" className="text-secondary font-semibold">About Us</Link>
+        </li>
+        <li>
+          <Link to="/recetas" className="text-secondary font-semibold">Recetas</Link>
+        </li>
+        <li>
+          <Link to="/order" className="text-secondary font-semibold">Realizar Pedido</Link>
+        </li>
       </ul>
+
+      {/* Efecto crayón debajo */}
+      <div className="absolute bottom-0 left-0 w-full h-6 bg-no-repeat bg-center bg-cover pointer-events-none"
+           style={{ backgroundImage: "url('/poster-mask.png')" }} />
     </nav>
-    );
+  );
 }
 
 export default Navbar;
